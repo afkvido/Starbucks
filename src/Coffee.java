54ded8af33b89771d83b0ea2a7ad7be74c4c77db
gemsvido <kid4cars@gmail.com>
2022-02-28 14:48:12 -0800

    Starbucks

--- src/Coffee.java
+++ src/Coffee.java
@@ -5,27 +5,32 @@ import resources.t;
 public class Coffee {
 
     public final Object general;
+    public final Object size;
     public final String customer;
     private Double subTotal;
     public Double total;
 
 
-    public Coffee (Object Type, String Customer) {
+
+    public Coffee (Object Type, Object Size, String Customer) {
         this.general = Type;
         this.customer = Customer;
+        this.size = Size;
     }
 
-    private void cost () {
-
-
+    private Double cost () {
         total = subTotal + ((subTotal * t.taxPercent) / 100);
-
+        return total;
     }
 
 
     @Override public @NotNull String toString () {
 
         return "e";
+    }
+
+    public void Make () {
+
     }
 
 
